d4d10d8d84fda6aca9296a71c1e1d52d801b3941
Musa-tech-ui <musalindokuhle6@gmail.com>
2024-08-21 11:29:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -16,13 +16,31 @@
   if (maléElement) {
     let maléDateElement = maléElement.querySelector(".date");
     let maléTimeElement = maléElement.querySelector(".time");
-    let maléTime = moment().tz("Asia/Malé");
+    let maléTime = moment().tz("Indian/Malé");
 
     maléDateElement.innerHTML = maléTime.format("MMMM	Do YYYY");
     maléTimeElement.innerHTML = maléTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
   updateTime();
 setInterval(updateTime, 1000);
